86847faaa6c7d1c08781bd5b7a6648cfefd46f69
Jacob Deppen <deppen.8@gmail.com>
2018-12-13 21:55:34 -0800

    refactor colors.py

--- colors.py
+++ colors.py
@@ -6,39 +6,93 @@
 # Secondary colors were used when it added diversity to the color palette.
 
 player_colors = {
-    'Barry Bonds': '#FD5A1E',  # SF
-    'Roger Clemens': '#BD3039',  # Boston
-    'Roy Halladay': '#006BA6',  # Toronto
-    'Todd Helton': '#8A8D8F',  # Colorado
-    'Andruw Jones': '#CE1141',  # Atlanta
-    'Edgar Martinez': '#005C5C',  # Seattle
-    'Fred McGriff': '#002B73',  # Tampa 
-    'Mike Mussina': '#0C2340',  # NYY
-    'Andy Pettitte': '#0C2340',  # NYY
-    'Manny Ramirez': '#BD3039' ,  # Boston
-    'Mariano Rivera': '#0C2340',  # NYY
-    'Scott Rolen': '#E81828',  # Philadelphia
-    'Curt Schilling': '#A71930',  # Arizona
-    'Gary Sheffield': '#009CA6', # Florida Marlins
-    'Sammy Sosa': '#0E3386',  # Chicago Cubs 
-    'Omar Vizquel': '#0C2340',  # Cleveland 
-    'Larry Walker': '#8A8D8F',  # Colorado,
-    'Miguel Tejada': '#EFB21E',  # Oakland
-    'Jeff Kent': '#FD5A1E',  # SF
-    'Lance Berkman': '#D3BC8D',  # Houston ('Sand')
-    'Roy Oswalt': '#D3BC8D',  # Houston ('Sand')
-    'Billy Wagner': '#D3BC8D',  # Houston ('Sand')
-    'Michael Young': '#C0111F',  # Texas
-    'Rick Ankiel': '#C41E3A',  # St. Louis
-    'Jason Bay': '#FDB827',  # Pittsburgh
-    'Freddy Garcia': '#005C5C',  # Seattle
-    'Jon Garland': '#C4CED4',  # ChiSox
-    'Travis Hafner': '#0C2340',  # Cleveland
-    'Ted Lilly': '#0E3386',  # Chicago Cubs
-    'Derek Lowe': '#BD3039' ,  # Boston
-    'Darren Oliver': '#C0111F',  # Texas
-    'Juan Pierre': '#009CA6', # Florida Marlins
-    'Placido Polanco': '#FA4616',  # Detroit
-    'Vernon Wells': '#006BA6',  # Toronto
-    'Kevin Youkilis': '#BD3039'  # Boston
+    '2019': {
+        'Barry Bonds': '#FD5A1E',  # SF
+        'Roger Clemens': '#BD3039',  # Boston
+        'Roy Halladay': '#006BA6',  # Toronto
+        'Todd Helton': '#8A8D8F',  # Colorado
+        'Andruw Jones': '#CE1141',  # Atlanta
+        'Edgar Martinez': '#005C5C',  # Seattle
+        'Fred McGriff': '#002B73',  # Tampa 
+        'Mike Mussina': '#0C2340',  # NYY
+        'Andy Pettitte': '#0C2340',  # NYY
+        'Manny Ramirez': '#BD3039' ,  # Boston
+        'Mariano Rivera': '#0C2340',  # NYY
+        'Scott Rolen': '#E81828',  # Philadelphia
+        'Curt Schilling': '#A71930',  # Arizona
+        'Gary Sheffield': '#009CA6', # Florida Marlins
+        'Sammy Sosa': '#0E3386',  # Chicago Cubs 
+        'Omar Vizquel': '#0C2340',  # Cleveland 
+        'Larry Walker': '#8A8D8F',  # Colorado,
+        'Miguel Tejada': '#EFB21E',  # Oakland
+        'Jeff Kent': '#FD5A1E',  # SF
+        'Lance Berkman': '#D3BC8D',  # Houston ('Sand')
+        'Roy Oswalt': '#D3BC8D',  # Houston ('Sand')
+        'Billy Wagner': '#D3BC8D',  # Houston ('Sand')
+        'Michael Young': '#C0111F',  # Texas
+        'Rick Ankiel': '#C41E3A',  # St. Louis
+        'Jason Bay': '#FDB827',  # Pittsburgh
+        'Freddy Garcia': '#005C5C',  # Seattle
+        'Jon Garland': '#C4CED4',  # ChiSox
+        'Travis Hafner': '#0C2340',  # Cleveland
+        'Ted Lilly': '#0E3386',  # Chicago Cubs
+        'Derek Lowe': '#BD3039' ,  # Boston
+        'Darren Oliver': '#C0111F',  # Texas
+        'Juan Pierre': '#009CA6', # Florida Marlins
+        'Placido Polanco': '#FA4616',  # Detroit
+        'Vernon Wells': '#006BA6',  # Toronto
+        'Kevin Youkilis': '#BD3039'  # Boston
+    },
+    '2018': {
+        'Barry Bonds': '#FD5A1E',  # SF
+        'Roger Clemens': '#BD3039',  # Boston
+        'Johnny Damon': '#BD3039',  # Boston
+        'Vlad Guerrero': '#E4002B',  # Montreal
+        'Trevor Hoffman': '#E35205',  # San Diego
+        'Andruw Jones': '#CE1141',  # Atlanta
+        'Chipper Jones': '#CE1141',  # Atlanta
+        'Jeff Kent': '#FD5A1E',  # SF
+        'Edgar Martinez': '#005C5C',  # Seattle
+        'Fred McGriff': '#002B73',  # Tampa 
+        'Mike Mussina': '#0C2340',  # NYY        
+        'Manny Ramirez': '#BD3039' ,  # Boston
+        'Scott Rolen': '#E81828',  # Philadelphia
+        'Johan Santana': '#002B5C',  # Minnesota
+        'Curt Schilling': '#A71930',  # Arizona
+        'Gary Sheffield': '#009CA6', # Florida Marlins
+        'Sammy Sosa': '#0E3386',  # Chicago Cubs 
+        'Jim Thome': '#0C2340',  # Cleveland
+        'Omar Vizquel': '#0C2340',  # Cleveland
+        'Billy Wagner': '#D3BC8D',  # Houston ('Sand')
+        'Larry Walker': '#8A8D8F',  # Colorado,
+        'Hideki Matsui': '#0C2340',  # NYY
+        'Jamie Moyer': '#005C5C',  # Seattle
+        'Kerry Wood': '#0E3386'  # Chicago Cubs 
+    },
+    '2017': {
+        'Jeff Bagwell': '#D3BC8D',  # Houston ('Sand')
+        'Barry Bonds': '#FD5A1E',  # SF
+        'Roger Clemens': '#BD3039',  # Boston
+        'Vlad Guerrero': '#E4002B',  # Montreal
+        'Trevor Hoffman': '#E35205',  # San Diego
+        'Jeff Kent': '#FD5A1E',  # SF
+        'Edgar Martinez': '#005C5C',  # Seattle
+        'Fred McGriff': '#002B73',  # Tampa 
+        'Mike Mussina': '#0C2340',  # NYY
+        'Jorge Posada': '#0C2340',  # NYY
+        'Tim Raines': '#E4002B',  # Montreal
+        'Manny Ramirez': '#BD3039',  # Boston
+        'Ivan Rodriguez': '#C0111F',  # Texas
+        'Curt Schilling': '#A71930',  # Arizona
+        'Gary Sheffield': '#009CA6', # Florida Marlins
+        'Lee Smith': '#0E3386',  # Chicago Cubs
+        'Sammy Sosa': '#0E3386',  # Chicago Cubs 
+        'Billy Wagner': '#D3BC8D',  # Houston ('Sand')
+        'Larry Walker': '#8A8D8F',  # Colorado
+        'Edgar Renteria': '#009CA6', # Florida Marlins
+        'Jason Varitek': '#BD3039',  # Boston
+        'Tim Wakefield': '#BD3039'  # Boston
+    }
 }
+
+
